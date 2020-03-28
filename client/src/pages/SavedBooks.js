f1255e8fc6c016d1bbfb493858b4da52e1dc896e
Kimia Darden <kimiadarden@gmail.com>
2020-03-28 12:34:08 -0400

    saving books for later

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, {Component } from "react";
+import React, { Component } from "react";
 import Results from "../components/Results";
 import API from "../utils/API";
 
@@ -6,7 +6,24 @@ class SavedBooks extends Component {
 
 
 
+  state = {
+    savedBooks: [],
+  }
 
+  componentDidMount() {
+    API.savedBooks()
+      .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h2>Saved books</h2>
+        <Results books={this.state.savedBooks} />
+      </div>
+    )
+  }
 
 
 
